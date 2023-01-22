c94a61cf091156e439db12f06d719146145f8442
bromcei <tomceid@gmail.com>
2023-01-22 19:09:28 +0200

    added some things

---
+++ DBExam/Repositories/ProductsRepository.cs
@@ -0,0 +1,34 @@
+﻿using DBExam.Classes;
+using DBExam.DbContextServer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBExam.Repositories
+{
+    public class ProductsRepository
+    {
+        public HoneyBadgerDbContext HoneyBadgerDB { get; set; }
+
+        public ProductsRepository()
+        {
+            HoneyBadgerDB = new HoneyBadgerDbContext();
+        }
+
+        public List<Product> Retrieve()
+        {
+            return HoneyBadgerDB.Suppliers.ToList();
+        }
+        public Product Retrieve(string productName)
+        {
+            return HoneyBadgerDB.Products.Where(s => s.ProductName == productName).FirstOrDefault();
+        }
+        public void AddNewProduct(Product product)
+        {
+            HoneyBadgerDB.Products.Add(product);
+            HoneyBadgerDB.SaveChanges();
+        }
+    }
+}
